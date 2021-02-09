1f8af1a782734f72d773e89f69275a0a193c243c
Chris <christophermoret@gmail.com>
2021-02-09 15:29:14 +0100

    nav-bar router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,87 @@
 import '../App.css';
 import logo from './img/roundtable.jpg';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function Header() {
     return (
       <div className="Header">
-        <nav class="nav-bar">
-          <button>Content</button>
-          <button>About us</button>
-        </nav>
+<Router>
+      <div className="nav-bar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
         <div>
           <img className="img-logo" src={logo} alt="Logo" />
         </div>
-        <p>Round Table</p>
       </div>
     );
   }
+
+  // You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
   
   export default Header;
   
