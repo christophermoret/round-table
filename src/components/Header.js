4b44e6ed2952f45e64f4348fc69f2d3fbfe54f46
Chris <christophermoret@gmail.com>
2021-02-11 22:15:47 +0100

    Ui navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import '../App.css';
 import holli from './img/holli.gif';
 import logo from '../logo.svg';
 import React from "react";
@@ -25,60 +24,13 @@ function Header() {
             <Link to="/dashboard">Dashboard</Link>
           </li>
         </ul>
-
-        <hr />
-
-        {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
       </div>
-    </Router>
-
-  </div>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <img className="App-logo" src={logo} alt="Logo" />
+      </Router>
     </div>
   );
 }
 
-function About() {
-  return (
-    <div>
-      <img className="App-holli" src={holli} alt="Logo" />
-    </div>
-  );
-}
 
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
 
 
 export default Header;
